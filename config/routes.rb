7c4e55a78c54313feea438726387360cab06e400
Yu Utsugi <utsugiu@gmail.com>
2019-09-02 13:58:08 +0900

    Created edit and update action

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   delete '/infos/:id' => 'infos#destroy'
   post '/callback' => 'infos#callback'
   get '/infos/:id/edit' => 'infos#edit'
+  patch "/infos/:id/edit"  => 'infos#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
